51c06c4954d0e273fab0be238451a9369e911aae
Salam337 <salamadash@gmail.com>
2023-02-14 14:04:40 -0500

    added backend with models

--- frontend/shop/src/components/screens/HomeScreen.js
+++ frontend/shop/src/components/screens/HomeScreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react' // useState useEffect to make axios call from backend data
 import { Row, Col } from 'react-bootstrap'
 import products from '../../products'
 import Product from '../Product'
+import axios from 'axios'
 
 function HomeScreen() {
+  const [products,setProducts] = useState([])
+
+  useEffect(() => {
+
+    async function fetchProducts() {
+
+      const { data } = await axios.get('/api/products/') //http://localhost:8000 inside package.json
+      setProducts(data)
+    }
+    fetchProducts()
+    
+  },[]) //empty array to make it load only when the component gets load
+
   return (
     <div>
         <h1> Latest Products</h1>
--- frontend/shop/src/components/screens/ProductScreen.js
+++ frontend/shop/src/components/screens/ProductScreen.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../Rating'
-import products from '../../products'
+import axios from 'axios'
+
+// import products from '../../products'
 
 function ProductScreen() {
     const product_id  = useParams();
-    const product = products.find((p) => p._id === product_id.id)
+    // const product = products.find((p) => p._id === product_id.id)
+
+    const [product,setProduct] = useState([])
+
+  useEffect(() => {
+
+    async function fetchProduct() {
+
+      const { data } = await axios.get(`/api/products/${product_id.id}`) //http://localhost:8000 inside package.json
+      setProduct(data)
+    }
+    fetchProduct()
+    
+  },[])
     return (
     <div>
         <Link to ='/' className='btn btn-light my-3'>Back</Link>
